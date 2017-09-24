18196a539844dd2dffc74bc3447b47b9640b359f
itacir gabral <degumes@users.noreply.github.com>
2017-09-23 23:31:28 -0400

    remove logs

--- run.js
+++ run.js
@@ -1,13 +1,6 @@
  document.addEventListener("DOMContentLoaded", ()=>{
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register("sw.js")
-      .then(function(reg) {
-        console.log('Registration succeeded')
-        console.dir(reg)
-      }).catch(function(error) {
-        console.log("Registration failed")
-        console.error(error)
-      })
+      navigator.serviceWorker.register("sw.js");
     }
 });
 Reveal.initialize({});
