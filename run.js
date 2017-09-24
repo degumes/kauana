e1aca4972bade79acfb3e5900168e07d6cac1e13
itacir gabral <degumes@users.noreply.github.com>
2017-09-23 23:18:05 -0400

    manifest.json && service worker

--- run.js
+++ run.js
@@ -1,1 +1,13 @@
- Reveal.initialize({});
+ document.addEventListener("DOMContentLoaded", ()=>{
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register("sw.js")
+      .then(function(reg) {
+        console.log('Registration succeeded')
+        console.dir(reg)
+      }).catch(function(error) {
+        console.log("Registration failed")
+        console.error(error)
+      })
+    }
+});
+Reveal.initialize({});
---
+++ sw.js
@@ -0,0 +1,32 @@
+this.addEventListener("install", e=>{
+    e.waitUntil(caches.open("23september2017").then(c=>{
+        return c.addAll([
+            "/",
+            "/stl.css",
+            "/run.js"
+        ])
+    }))
+})
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('23september2017').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+this.addEventListener("activate", e=>{
+    const lastCache = "23september2017"
+    e.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(keys.map(k=>{
+                if(k !== lastCache){
+                    return caches.delete(k)
+                }
+            }))
+    }))
+})
